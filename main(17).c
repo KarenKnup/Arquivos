5ab17609e24b5e835972feba2ee88f7962c99b57
KarenKnup <106675809+KarenKnup@users.noreply.github.com>
2022-12-04 10:23:17 -0300

    Add files via upload

---
+++ main(17).c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define TRUE 0
+#define FALSE 1
+
+ int busca(char nomeArq[], int l){
+  FILE *arq;
+  int n, cont=1;
+
+    arq=fopen(nomeArq,"r");
+
+  if(!arq){
+    return FALSE;
+  } else {
+    while (fscanf(arq,"%d",&n)!=EOF){
+      if (cont==l){
+        fclose(arq);
+        return n;
+      }
+      cont++;
+    }
+  }
+}
+
+int main(void) {
+  char nomeArq[20];
+  int l;
+
+  //Lendo o nome do arquivo
+  printf("Arquivo: ");
+  scanf(" %[^\n]",nomeArq);
+  printf("Linha: ");
+  scanf("%d",&l);
+
+  printf("%d",busca(nomeArq,l));
+  
+  return 0;
+}
